807689d85bc4101059dac71d061b950a5067c96a
edmilson-dk <droidvidaboa@gmail.com>
2021-03-19 11:07:36 -0300

    feat: create todo test file

---
+++ test/todo.test.js
@@ -0,0 +1,14 @@
+const { describe, it, before } = require('mocha');
+const { expect } = require('chai');
+
+describe('Todo', () => {
+  describe('#isValid', () => {
+    it('should return invalid when creating an object without text', () => {
+      const data = {
+        
+      }
+    });
+    it('should return invalid when creating an object using the "when" property invalid');
+    it('should have "id", "text", "when" and "status" properties after creating object');
+  })
+})
